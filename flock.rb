3e05bcb6c4cc1fb5acfa7a12a3bab1379c46a260
Jake Heiser <jakeheiser1@gmail.com>
2016-11-23 15:28:41 -0600

    Bumping version

--- flock.rb
+++ flock.rb
@@ -2,7 +2,7 @@ class Flock < Formula
   desc ""
   homepage ""
   url "https://github.com/jakeheis/FlockCLI/archive/0.0.1.tar.gz"
-  version "0.0.1"
+  version "0.0.3"
   sha256 "0ec32506edecfe7c44a529a8d0c2eec47731ee9d944d5b3e092b4b0c11f3b647"
 
   def install
