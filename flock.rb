a621a0d31f9221401dcc222ef6de745dfc5669da
Jake Heiser <jakeheiser1@gmail.com>
2017-06-16 10:48:06 -0500

    Updating flock version

--- flock.rb
+++ flock.rb
@@ -1,8 +1,8 @@
 class Flock < Formula
   desc ""
   homepage ""
-  url "https://github.com/jakeheis/FlockCLI/archive/0.1.1.tar.gz"
-  version "0.1.1"
+  url "https://github.com/jakeheis/FlockCLI/archive/0.2.0.tar.gz"
+  version "0.2.0"
   sha256 "3f2da59ba2f66309ddba03cf51eea09b4fbac424084235fd530681bb05c2e6d0"
 
   def install
