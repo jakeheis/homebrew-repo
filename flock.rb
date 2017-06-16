4b308e4889c01141c138e7c8a173fdb593f4f39b
Jake Heiser <jakeheiser1@gmail.com>
2017-06-16 10:48:53 -0500

    Fixed checksum

--- flock.rb
+++ flock.rb
@@ -3,7 +3,7 @@ class Flock < Formula
   homepage ""
   url "https://github.com/jakeheis/FlockCLI/archive/0.2.0.tar.gz"
   version "0.2.0"
-  sha256 "3f2da59ba2f66309ddba03cf51eea09b4fbac424084235fd530681bb05c2e6d0"
+  sha256 "fb3f0a59ca3abe17063aefb3f14e8f979b46b480948e18369d59c9063b0bccee"
 
   def install
     system "unset CC; swift build -c release"
