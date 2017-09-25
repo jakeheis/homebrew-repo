4b313f2011d66fa786d8d1d453ef15020d8caf1a
Jake Heiser <jakeheiser1@gmail.com>
2017-09-25 15:28:30 -0700

    Add ice checksum

--- ice.rb
+++ ice.rb
@@ -3,6 +3,7 @@ class Ice < Formula
   homepage ""
   url "https://github.com/jakeheis/Ice/archive/0.1.0.tar.gz"
   version "0.1.0"
+  sha256 "1f1b10c6c24388214d79b78bb4276eb2af32c5b17c51caed16cec6d4773b2ea8"
 
   def install
     system "unset CC; swift build -c release"
