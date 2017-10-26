19ac7f5cac5370e357c25b14aa7a2abb4a1e6638
Jake Heiser <jakeheiser1@gmail.com>
2017-10-25 21:48:07 -0700

    Try fix with disable sandbox

--- flock.rb
+++ flock.rb
@@ -6,7 +6,7 @@ class Flock < Formula
   sha256 "a93f25796c2896cd7207310c99752bc25df99870bcac201c2d082880d26d3a8c"
 
   def install
-    system "unset CC; swift build -c release"
+    system "unset CC; swift build --disable-sandbox -c release"
     system "mv .build/release/FlockCLI flock"
     bin.install "flock"
   end
