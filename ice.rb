850f7a65f4ad7d7806d7f1df554594bc1b57e005
Jake Heiser <jakeheiser1@gmail.com>
2017-10-25 21:53:18 -0700

    Disable sandbox in swift build

--- ice.rb
+++ ice.rb
@@ -1,12 +1,12 @@
 class Ice < Formula
   desc "A developer friendly package manager for Swift; 100% compatible with Swift Package Manager"
   homepage ""
-  url "https://github.com/jakeheis/Ice/archive/0.1.0.tar.gz"
-  version "0.1.0"
-  sha256 "1f1b10c6c24388214d79b78bb4276eb2af32c5b17c51caed16cec6d4773b2ea8"
+  url "https://github.com/jakeheis/Ice/archive/0.1.1.tar.gz"
+  version "0.1.1"
+  sha256 "ccf7c55818cb8e43d18bae907a4c0c5c7df3e07aa5e5dda8744c696417f301dc"
 
   def install
-    system "unset CC; swift build -c release"
+    system "unset CC; swift build --disable-sandbox -c release"
     system "mv .build/release/ice ice"
     bin.install "ice"
   end
