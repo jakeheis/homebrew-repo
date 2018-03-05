d848af9d9613b10c68d2be6e559c3fd6b16b705a
Jake Heiser <jakeheiser1@gmail.com>
2018-03-05 11:54:43 -0600

    Update ice

--- ice.rb
+++ ice.rb
@@ -1,9 +1,9 @@
 class Ice < Formula
   desc "A developer friendly package manager for Swift; 100% compatible with Swift Package Manager"
   homepage ""
-  url "https://github.com/jakeheis/Ice/archive/0.1.1.tar.gz"
-  version "0.1.1"
-  sha256 "ccf7c55818cb8e43d18bae907a4c0c5c7df3e07aa5e5dda8744c696417f301dc"
+  url "https://github.com/jakeheis/Ice/archive/0.3.0.tar.gz"
+  version "0.3.0"
+  sha256 "390b31caae2f3668260e856ff036b2b0bceb8060e3aeacd1661cc736cf1058d2"
 
   def install
     system "unset CC; swift build --disable-sandbox -c release"
